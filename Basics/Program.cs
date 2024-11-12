a6969dd29ffc0c0777c2e22208b46bc58de09147
Alex Bondar <abondar1992@gmail.com>
2024-11-12 09:06:00 +0100

    base console app

---
+++ Basics/Program.cs
@@ -0,0 +1,29 @@
+﻿   using System;
+
+   namespace Basics;
+
+
+   //TODO: refactor to accept arguments
+   //TODO: refactor to have a proper help  
+   //TODO: refactor to proper classes
+
+   class Program { static void Main(string[] args) {
+      Console.WriteLine("C# is on");
+
+      DrawShape();
+    
+   }
+
+   private static void DrawShape(){
+     Console.WriteLine("   /|");
+     Console.WriteLine("  / |");
+     Console.WriteLine(" /  |");
+     Console.WriteLine("/___|");
+     
+    
+   }
+
+    
+   }
+
+
